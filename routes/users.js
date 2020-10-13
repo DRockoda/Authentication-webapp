09633664bdff833310e480558fc53036779ec590
DRockoda <70942337+DRockoda@users.noreply.github.com>
2020-10-13 13:01:29 +0530

    fixing messages

--- routes/users.js
+++ routes/users.js
@@ -101,7 +101,7 @@ router.post('/login',(req,res,next) => {
 
 router.get('/logout',(req,res) =>{
     req.logout()
-    req.flash('success_msg','Yu are logged out')
+    req.flash('success_msg','You are logged out')
     res.redirect('/users/login')
 })
 
